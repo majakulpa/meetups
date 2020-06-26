6856f8c8b68d6aeebd89e9feb5a4b95e67bcf254
majakulpa <majaweronikakulpa@gmail.com>
2020-06-27 00:27:33 +1000

    add axios, connect backend

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,10 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/events'
 
 class Events extends Component {
-  state = {};
+  state = {
+    events: []
+  }
+
+  componentDidMount() {
+    const request = axios.get(baseUrl)
+    request.then(res => {
+      const events = res.data
+      this.setState({ events })
+    })
+  }
+
   render() {
-    return <div>Events</div>;
+    return (
+      <div>
+        <ul>
+          {this.state.events.map(event => (
+            <li key={event.id}>{event.title}</li>
+          ))}
+        </ul>
+      </div>
+    )
   }
 }
 
-export default Events;
+export default Events
