597d7855b05d35b144a4cd3b5cc102842b15fb1a
majakulpa <majaweronikakulpa@gmail.com>
2020-10-18 02:38:55 +1100

    add hashrouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './assets/main.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 import './index.css'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 )
 
