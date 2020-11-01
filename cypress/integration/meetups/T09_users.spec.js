7c39701ff5fcd674e0d8df732d23dba5aec4cf38
majakulpa <majaweronikakulpa@gmail.com>
2020-11-01 15:55:21 +1100

    added test

--- cypress/integration/meetups/T09_users.spec.js
+++ cypress/integration/meetups/T09_users.spec.js
@@ -3,9 +3,14 @@ context('Signed user can view other users profiles', function() {
         cy.SignIn()
     })
     
+    it('can view other user\'s profile', function() {
+        cy.get('input#search').type('create your own')
+        cy.contains('Create your own', {timeout: 10000}).should('be.visible').click()
+        cy.contains('Attendees', {timeout: 10000}).should('be.visible')
+        cy.get('.flex-wrap a.flex.flex-col').first().click()
+        cy.url().should('include', '/users/')
+        cy.get('a.bg-purple-600').contains('Send email', {timeout: 10000}).should('be.visible')
+      })
 
-  
- 
-  
   })
   
